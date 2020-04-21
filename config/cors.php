679f0b53c8d6abf9a7846f29f970246cf4ed7ee8
blackblue2712 <lars27399@gmail.com>
2020-04-21 22:31:21 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -56,5 +56,16 @@ return [
     /*
      * Sets the Access-Control-Allow-Credentials header.
      */
-    'supports_credentials' => false,
+    'supports_credentials' => true,
+
+
+
+    // 'paths' => [],
+    // 'allowed_methods' => ['*'],
+    // 'allowed_origins' => ['*'],
+    // 'allowed_origins_patterns' => [],
+    // 'allowed_headers' => ['*'],
+    // 'exposed_headers' => false,
+    // 'max_age' => false,
+    // 'supports_credentials' => false,
 ];
